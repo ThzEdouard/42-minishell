caf54369bffcf735b5a0bdf824e7e1175403f7a2
ThzEdouard <edouard.flaquet@gmail.com>
2022-10-15 09:51:20 +0200

    p

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,1 +1,14 @@
-#include "../../minitest.h
+#include "../../include/minitest.h"
+
+void    init(t_list_token *l)
+{
+    l->first = NULL;
+    l->lest = NULL;
+}
+
+
+
+int     parsing(char *line)
+{
+    return (SUCCES);
+}
