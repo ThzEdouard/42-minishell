a3fdcc98eb7d488c187ef65751412e403ab8bf39
ThzEdouard <edouard.flaquet@gmail.com>
2022-11-03 15:35:59 +0100

    r

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: eflaquet <eflaquet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 11:41:23 by eflaquet          #+#    #+#             */
-/*   Updated: 2022/11/03 11:50:25 by eflaquet         ###   ########.fr       */
+/*   Updated: 2022/11/03 15:24:18 by eflaquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,9 @@ int main(int argc, char **argv, char **envp)
 	(void) argc, (void)argv;
 	t_env	*env;
 	generator_env(&env, envp);
-	
+
 	prompt(env);
+
+	//cler ce qu'il y a a clear
 	return (SUCCESS);
 }
